9e2ab63e830f7d753b106e29c4df38913ed69852
sejimhp <sejimhp@gmail.com>
2015-05-04 18:41:59 +0900

    前後10個のピース

--- procon26_kyogi/Form1.Designer.cs
+++ procon26_kyogi/Form1.Designer.cs
@@ -30,11 +30,16 @@
     {
       this.tabControl1 = new System.Windows.Forms.TabControl();
       this.tabPage1 = new System.Windows.Forms.TabPage();
-      this.tabPage2 = new System.Windows.Forms.TabPage();
       this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.button2 = new System.Windows.Forms.Button();
+      this.button1 = new System.Windows.Forms.Button();
+      this.pictureBox2 = new System.Windows.Forms.PictureBox();
       this.tabControl1.SuspendLayout();
-      this.tabPage2.SuspendLayout();
+      this.tabPage1.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.tabPage2.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
       this.SuspendLayout();
       // 
       // tabControl1
@@ -44,31 +49,20 @@
       this.tabControl1.Location = new System.Drawing.Point(12, 5);
       this.tabControl1.Name = "tabControl1";
       this.tabControl1.SelectedIndex = 0;
-      this.tabControl1.Size = new System.Drawing.Size(1160, 594);
+      this.tabControl1.Size = new System.Drawing.Size(1160, 644);
       this.tabControl1.TabIndex = 0;
       // 
       // tabPage1
       // 
+      this.tabPage1.Controls.Add(this.pictureBox1);
       this.tabPage1.Location = new System.Drawing.Point(4, 22);
       this.tabPage1.Name = "tabPage1";
       this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage1.Size = new System.Drawing.Size(1152, 568);
+      this.tabPage1.Size = new System.Drawing.Size(1152, 618);
       this.tabPage1.TabIndex = 0;
       this.tabPage1.Text = "Main";
       this.tabPage1.UseVisualStyleBackColor = true;
       this.tabPage1.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage1_Paint);
-      // 
-      // tabPage2
-      // 
-      this.tabPage2.Controls.Add(this.pictureBox1);
-      this.tabPage2.Location = new System.Drawing.Point(4, 22);
-      this.tabPage2.Name = "tabPage2";
-      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage2.Size = new System.Drawing.Size(1152, 568);
-      this.tabPage2.TabIndex = 1;
-      this.tabPage2.Text = "Edit";
-      this.tabPage2.UseVisualStyleBackColor = true;
-      this.tabPage2.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage2_Paint);
       // 
       // pictureBox1
       // 
@@ -78,20 +72,64 @@
       this.pictureBox1.TabIndex = 0;
       this.pictureBox1.TabStop = false;
       // 
+      // tabPage2
+      // 
+      this.tabPage2.Controls.Add(this.button2);
+      this.tabPage2.Controls.Add(this.button1);
+      this.tabPage2.Controls.Add(this.pictureBox2);
+      this.tabPage2.Location = new System.Drawing.Point(4, 22);
+      this.tabPage2.Name = "tabPage2";
+      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage2.Size = new System.Drawing.Size(1152, 618);
+      this.tabPage2.TabIndex = 1;
+      this.tabPage2.Text = "Edit";
+      this.tabPage2.UseVisualStyleBackColor = true;
+      this.tabPage2.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage2_Paint);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(618, 562);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(100, 40);
+      this.button2.TabIndex = 4;
+      this.button2.Text = "スキップ";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(423, 562);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(100, 40);
+      this.button1.TabIndex = 3;
+      this.button1.Text = "戻る";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // pictureBox2
+      // 
+      this.pictureBox2.Location = new System.Drawing.Point(6, 0);
+      this.pictureBox2.Name = "pictureBox2";
+      this.pictureBox2.Size = new System.Drawing.Size(1140, 622);
+      this.pictureBox2.TabIndex = 0;
+      this.pictureBox2.TabStop = false;
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.BackColor = System.Drawing.SystemColors.Window;
-      this.ClientSize = new System.Drawing.Size(1184, 611);
+      this.ClientSize = new System.Drawing.Size(1184, 661);
       this.Controls.Add(this.tabControl1);
       this.Name = "Form1";
       this.Tag = "";
       this.Text = "procon26_kyogi";
       this.Load += new System.EventHandler(this.Form1_Load);
       this.tabControl1.ResumeLayout(false);
-      this.tabPage2.ResumeLayout(false);
+      this.tabPage1.ResumeLayout(false);
       ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.tabPage2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
       this.ResumeLayout(false);
 
     }
@@ -102,6 +140,9 @@
     private System.Windows.Forms.TabPage tabPage1;
     private System.Windows.Forms.TabPage tabPage2;
     private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.PictureBox pictureBox2;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button1;
   }
 }
 
--- procon26_kyogi/Form1.cs
+++ procon26_kyogi/Form1.cs
@@ -16,14 +16,109 @@ namespace procon26_kyogi
     {
       InitializeComponent();
     }
-
+    //定数
     const int MAP = 32, width = 17, length = 17;
+
+    int pieces = 10;
+    int count = 0;
+    //Fontを作成
+    Font fnt = new Font("ＭＳ ゴシック", 12);
     int[,] map = new int[32, 32];
+    int[, ,] item = new int[10, 8, 8] { { { 0,0,0,0,0,0,0,0},
+                                        { 0,1,1,1,1,1,1,0},
+                                        { 0,1,0,0,0,0,0,0},
+                                        { 0,1,0,0,0,0,0,0}, 
+                                        { 0,1,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0}, 
+                                        { 0,0,0,0,0,0,0,0}, 
+                                        { 0,0,0,0,0,0,0,0} },
+    
+                                        { { 0,1,0,0,0,0,0,0},
+                                        { 0,1,0,0,0,0,0,0},
+                                        { 0,1,0,0,0,0,0,0},
+                                        { 0,1,0,0,0,0,0,0}, 
+                                        { 0,1,0,0,0,0,0,0},
+                                        { 0,1,0,0,0,0,0,0}, 
+                                        { 0,1,0,0,0,0,0,0}, 
+                                        { 0,1,0,0,0,0,0,0} },
+    
+                                        { { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,1,0,0,0},
+                                        { 0,0,0,0,1,0,0,0}, 
+                                        { 0,0,0,0,1,1,1,0},
+                                        { 0,0,0,0,0,0,1,0}, 
+                                        { 0,0,0,0,0,0,1,0}, 
+                                        { 0,0,0,0,0,0,0,0} },
+    
+                                        { { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0}, 
+                                        { 0,0,0,1,1,1,1,0},
+                                        { 0,0,0,1,1,1,1,0}, 
+                                        { 0,0,0,1,1,1,1,0}, 
+                                        { 0,0,0,0,0,0,0,0} },
+
+                                        { { 0,0,0,0,0,0,0,1},
+                                        { 0,0,0,0,0,0,1,1},
+                                        { 0,0,0,0,0,1,1,0},
+                                        { 0,0,0,0,1,1,0,0}, 
+                                        { 0,0,0,1,1,0,0,0},
+                                        { 0,0,1,1,0,0,0,0}, 
+                                        { 0,1,1,0,0,0,0,0}, 
+                                        { 1,1,0,0,0,0,0,0} },
+    
+                                        { { 0,0,0,0,0,0,0,0},
+                                        { 1,1,0,0,0,0,0,0},
+                                        { 1,0,0,0,0,0,0,0},
+                                        { 1,0,0,0,0,0,0,0}, 
+                                        { 1,1,1,1,1,1,1,1},
+                                        { 0,0,0,0,0,0,0,1}, 
+                                        { 0,0,0,0,0,0,0,1}, 
+                                        { 0,0,0,0,0,0,0,1} },
+    
+                                        { { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,1,0,1,0,0},
+                                        { 0,0,1,1,1,1,1,0}, 
+                                        { 0,0,1,0,0,0,0,0},
+                                        { 0,0,1,1,1,0,0,0}, 
+                                        { 0,0,0,0,0,0,0,0}, 
+                                        { 0,0,0,0,0,0,0,0} },
+    
+                                        { { 0,0,0,0,0,0,0,0},
+                                        { 0,0,1,0,0,0,0,0},
+                                        { 0,0,1,0,0,0,0,0},
+                                        { 0,0,1,0,0,0,0,0}, 
+                                        { 0,0,1,1,1,1,0,0},
+                                        { 0,0,1,0,0,0,0,0}, 
+                                        { 0,0,1,0,0,0,0,0}, 
+                                        { 0,0,0,0,0,0,0,0} },
+    
+                                        { { 0,0,0,0,1,0,0,0},
+                                        { 0,1,1,1,1,1,0,0},
+                                        { 0,0,0,0,0,1,0,0},
+                                        { 0,0,1,0,0,1,0,0}, 
+                                        { 0,0,1,0,0,1,0,0},
+                                        { 0,0,1,1,1,1,0,0}, 
+                                        { 0,0,0,0,0,0,0,0}, 
+                                        { 0,0,0,0,0,0,0,0} },
+    
+                                        { { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0}, 
+                                        { 0,0,0,0,0,0,0,0},
+                                        { 0,0,0,0,0,0,0,0}, 
+                                        { 0,0,1,1,1,1,0,0}, 
+                                        { 0,0,1,1,1,1,1,0} },};
 
 
     //初期化やファイル読み込み
     private void Form1_Load(object sender, EventArgs e)
     {
+      //テスト用のフィールド
       for (int i = 0; i < MAP; i++)
         for (int j = 0; j < MAP; j++)
           map[i, j] = 0;
@@ -35,34 +130,31 @@ namespace procon26_kyogi
       }
       map[10, 10] = 1;
 
+      //テスト用にパーツの作成
+
 
     }
 
     private void tabPage1_Paint(object sender, PaintEventArgs e)
     {
-
-    }
-
-    private void tabPage2_Paint(object sender, PaintEventArgs e)
-    {
       //*************枠の作成*****************
       //描画先とするImageオブジェクトを作成する
-      Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+      Bitmap canvas = new Bitmap(pictureBox2.Width, pictureBox1.Height);
       //ImageオブジェクトのGraphicsオブジェクトを作成する
       Graphics g = Graphics.FromImage(canvas);
       for (int i = 0; i <= MAP; i++)
       {
         //(x, y)-(x, y)に、幅1の黒い線を引く
-        g.DrawLine(Pens.Black, (300), i * length, (300 + 32 * width), i * length);
-        g.DrawLine(Pens.Black, (300 + i * width), 0, (300 + i * width), 32 * length);
+        g.DrawLine(Pens.Black, (800), 250 + i * length / 2, (800 + 32 * width / 2), 250 + i * length / 2);
+        g.DrawLine(Pens.Black, (800 + i * width / 2), 250, (800 + i * width / 2), 250 + 32 * length / 2);
       }
       for (int i = 0; i < MAP; i++)
       {
         for (int j = 0; j < MAP; j++)
         {
           //黒色の表示
-          if(map[i, j] == 1)
-            g.FillRectangle(Brushes.Black, (300 + j * width), i * length, width, length);
+          if (map[i, j] == 1)
+            g.FillRectangle(Brushes.Black, (800 + j * width / 2), 250 + i * length / 2, 1 + width / 2, 1 + length / 2);
         }
       }
       //リソースを解放する
@@ -70,11 +162,131 @@ namespace procon26_kyogi
       //PictureBox1に表示する
       pictureBox1.Image = canvas;
       //**************************************
+    
+    }
 
+    private void tabPage2_Paint(object sender, PaintEventArgs e)
+    {
+      //*************枠の作成*****************
+      //描画先とするImageオブジェクトを作成する
+      Bitmap canvas = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+      //ImageオブジェクトのGraphicsオブジェクトを作成する
+      Graphics g = Graphics.FromImage(canvas);
+      for (int i = 0; i <= MAP; i++)
+      {
+        //(x, y)-(x, y)に、幅1の黒い線を引く
+        g.DrawLine(Pens.Black, (275), i * length, (275 + 32 * width), i * length);
+        g.DrawLine(Pens.Black, (275 + i * width), 0, (275 + i * width), 32 * length);
+      }
+      for (int i = 0; i < MAP; i++)
+      {
+        for (int j = 0; j < MAP; j++)
+        {
+          //黒色の表示
+          if(map[i, j] == 1)
+            g.FillRectangle(Brushes.Black, (275 + j * width), i * length, width, length);
+        }
+      }
+
+      //右ブロックの表示
+      for (int i = 0; i < 4; i++)
+      for (int j = 0; j <= 8; j++)
+      {
+        //(x, y)-(x, y)に、幅1の黒い線を引く
+        g.DrawLine(Pens.Black, (850), i * 160 + j * length, (850 + 8 * width), i * 160 + j * length);
+        g.DrawLine(Pens.Black, (850 + j * width), i * 160, (850 + j * width), i * 160 + 8 * length);
+      }
+      for (int i = 0; i < 6; i++)
+        for (int j = 0; j <= 8; j++)
+        {
+          //(x, y)-(x, y)に、幅1の黒い線を引く
+          g.DrawLine(Pens.Black, (1025), i * 100 + j * 10, (1025 + 8 * 10), i * 100 + j * 10);
+          g.DrawLine(Pens.Black, (1025 + j * 10), i * 100, (1025 + j * 10), i * 100 + 8 * 10);
+        }
+      for (int i = 0; i < 4; i++)
+      {
+        if (pieces > count + i)
+        for (int j = 0; j < 8; j++)
+          for (int k = 0; k < 8; k++)
+          if (item[count + i, j, k] == 1)
+            g.FillRectangle(Brushes.Aqua, (850 + k * width + 1), i * 160 + j * length + 1, width-1, length-1);
+      }
+      for (int i = 0; i < 6; i++)
+      {
+        if(pieces > count + 4 + i)
+        for (int j = 0; j < 8; j++)
+          for (int k = 0; k < 8; k++)
+            if (item[count + 4 + i, j, k] == 1)
+              g.FillRectangle(Brushes.Aqua, (1025 + k * 10 + 1), i * 100 + j * 10 + 1, 9, 9);
+      }
+
+      //左ブロックの表示
+      for (int i = 0; i < 10; i++)
+        for (int j = 0; j <= 8; j++)
+        {
+          //(x, y)-(x, y)に、幅1の黒い線を引く
+          g.DrawLine(Pens.Black, (15 + i / 5 * 125), i % 5 * 125 + j * 13, (15 + i / 5 * 125 + 8 * 13), i % 5 * 125 + j * 13);
+          g.DrawLine(Pens.Black, (15 + i / 5 * 125 + j * 13), i % 5 * 125, (15 + i / 5 * 125 + j * 13), i % 5 * 125 + 8 * 13);
+        }
+      for (int i = 0; i < 10; i++)
+      {
+        if ((count - 10 + i >= 0) && (pieces > count + i - 10))
+        for (int j = 0; j < 8; j++)
+          for (int k = 0; k < 8; k++)
+            if (item[count - 10 + i, j, k] == 1)
+              g.FillRectangle(Brushes.Aqua, (15 + i / 5 * 125 +  k * 13 + 1), i % 5 * 125 + j * 13 + 1, 12, 12);
+      }
+
+        //リソースを解放する
+        g.Dispose();
+      //PictureBox1に表示する
+      pictureBox2.Image = canvas;
+      //**************************************
+
+
+      /*ネタ*/
+      /*
+      t++;
+      t %= 1000;
+      if(t < 500){
+      //フォーム上の座標でマウスポインタの位置を取得する
+      //画面座標でマウスポインタの位置を取得する
+      System.Drawing.Point sp = System.Windows.Forms.Cursor.Position;
+      //画面座標をクライアント座標に変換する
+
+      System.Drawing.Point cp = this.PointToClient(sp);
+      //X座標を取得する
+      int x = cp.X;
+      //Y座標を取得する
+      int y = cp.Y;
+      float angle = (float)(0.02 * Math.PI * (t % 1000));
+      int a = (int)(250 + 200 * Math.Sin(angle));
+
+      //フォーム上の座標 (10, 20) にマウスポインタを移動する
+      //クライアント座標を画面座標に変換する
+      System.Drawing.Point mp = this.PointToScreen(
+          new System.Drawing.Point((t % 200) * 5, a));
+      //マウスポインタの位置を設定する
+      System.Windows.Forms.Cursor.Position = mp;
+      }*/
 
     }
 
+    //戻るボタン
+    private void button1_Click(object sender, EventArgs e)
+    {
+      if(count > 0)
+      count--;
+    }
 
+    //スキップボタン
+    private void button2_Click(object sender, EventArgs e)
+    {
+      if(count < (pieces + 10))
+      count++;
+    }
+
+    
     
   }
 }
